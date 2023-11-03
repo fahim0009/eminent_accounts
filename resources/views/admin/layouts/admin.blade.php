3ed0f59077ce762d9c2b59aa6a0c45b1830f2fd9
shakil <kazimuhammadullah@gmail.com>
2023-11-03 23:52:20 +0600

    accounts

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -59,6 +59,18 @@
     <ul class="navbar-nav ml-auto">
 
       
+      <li class="nav-item d-none d-sm-inline-block">
+        <a class="dropdown-item" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+                          document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
+      </li>
+      
       <li class="nav-item">
         <a class="nav-link" data-widget="fullscreen" href="#" role="button">
           <i class="fas fa-expand-arrows-alt"></i>
